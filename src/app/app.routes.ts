86cff1966bb8b1997e007134f79a6a9227a787d2
DADCARPER <davidcarvajalsistemas@outlook.com>
2024-05-21 15:51:21 -0500

    Plantilla configurada con Pagina Usuario y perfil Lista para trabajar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 export const routes: Routes = [
+
+    { path: '', component: DashboardComponent },
+    { path: 'perfil', component: PerfilComponent },
+    { path: 'usuarios', component: UsuariosComponent },
+    { path: '**', redirectTo: '', pathMatch:'full' }  
 
 ];
